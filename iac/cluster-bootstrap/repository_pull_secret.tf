1cb60e4294a5050842510f4bf9ced61e7dccdbc4
Cem Anaral <cemanaral425@gmail.com>
2025-06-07 15:17:48 +0300

    separate bootstrap tf files

---
+++ iac/cluster-bootstrap/repository_pull_secret.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_secret" "repository_pull_secret" {
+  metadata {
+    name = "dockerhub-image-pull-secret"
+    namespace = local.namespace
+  }
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "https://index.docker.io/v1/" = {
+          "username" = "cemanaral425383"
+          "password" = file(".dockerhub-token")
+        }
+      }
+    })
+  }
+}
--- iac/cluster-bootstrap/test_dockerhub_auth.tf
+++ iac/cluster-bootstrap/test_dockerhub_auth.tf
@@ -2,24 +2,6 @@ locals {
   namespace = "app"
 }
 
-resource "kubernetes_secret" "repository_pull_secret" {
-  metadata {
-    name = "dockerhub-image-pull-secret"
-    namespace = local.namespace
-  }
-  type = "kubernetes.io/dockerconfigjson"
-
-  data = {
-    ".dockerconfigjson" = jsonencode({
-      auths = {
-        "https://index.docker.io/v1/" = {
-          "username" = "cemanaral425383"
-          "password" = file(".dockerhub-token")
-        }
-      }
-    })
-  }
-}
 
 resource "kubernetes_namespace" "app_namespace" {
   metadata {
