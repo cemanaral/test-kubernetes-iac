c79f6a2c636fdbe89fcea0fa6967b8d4bdcca74c
Cem Anaral <cemanaral@hotmail.com>
2025-06-08 21:51:59 +0300

    change argocd app name

--- cluster-bootstrap/argocd.tf
+++ cluster-bootstrap/argocd.tf
@@ -24,7 +24,7 @@ resource "kubernetes_manifest" "argocd_application" {
     apiVersion = "argoproj.io/v1alpha1"
     kind       = "Application"
     metadata = {
-      name      = "argocd-app"
+      name      = "test-app"
       namespace = "argocd"
     }
     spec = {
