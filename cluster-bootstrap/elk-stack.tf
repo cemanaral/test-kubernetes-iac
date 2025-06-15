70ea5168161407c5fce7724161b0fbfe05d55d69
Cem Anaral <cem.anaral@commencis.com>
2025-06-16 00:08:08 +0300

    use logstash instead of fluentd/bit

--- cluster-bootstrap/elk-stack.tf
+++ cluster-bootstrap/elk-stack.tf
@@ -1,10 +1,10 @@
 locals {
-  efk_namespace = "efk"
+  elk_namespace = "elk"
 }
 
-resource "kubernetes_namespace" "efk" {
+resource "kubernetes_namespace" "elk" {
   metadata {
-    name = local.efk_namespace
+    name = local.elk_namespace
   }
 }
 
@@ -12,20 +12,31 @@ resource "helm_release" "elasticsearch" {
   name             = "elasticsearch"
   repository       = "https://helm.elastic.co"
   create_namespace = false
-  namespace        = local.efk_namespace
+  namespace        = local.elk_namespace
   chart            = "elasticsearch"
   version          = "8.5.1"
   values           = [file("./elasticsearch-values.yaml")]
-  depends_on = [ kubernetes_namespace.efk ]
+  depends_on = [ kubernetes_namespace.elk ]
 }
 
 resource "helm_release" "kibana" {
   name             = "kibana"
   repository       = "https://helm.elastic.co"
   create_namespace = false
-  namespace        = local.efk_namespace
+  namespace        = local.elk_namespace
   chart            = "kibana"
   version          = "8.5.1"
   values           = [file("./kibana-values.yaml")]
+  depends_on = [ helm_release.elasticsearch ]
+}
+
+resource "helm_release" "logstash" {
+  name             = "logstash"
+  repository       = "https://helm.elastic.co"
+  create_namespace = false
+  namespace        = local.elk_namespace
+  chart            = "logstash"
+  version          = "8.5.1"
+  values           = [file("./logstash-values.yaml")]
   depends_on = [ helm_release.elasticsearch ]
 }
