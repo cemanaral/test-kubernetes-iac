c51e19c0ebc68a31da20ee73ba6f45baf28f6e87
Cem Anaral <cem.anaral@commencis.com>
2025-06-16 20:28:30 +0300

    configure filebeat for log forwarding

--- cluster-bootstrap/efk-stack.tf
+++ cluster-bootstrap/efk-stack.tf
@@ -1,10 +1,10 @@
 locals {
-  elk_namespace = "elk"
+  efk_namespace = "efk"
 }
 
-resource "kubernetes_namespace" "elk" {
+resource "kubernetes_namespace" "efk" {
   metadata {
-    name = local.elk_namespace
+    name = local.efk_namespace
   }
 }
 
@@ -12,31 +12,31 @@ resource "helm_release" "elasticsearch" {
   name             = "elasticsearch"
   repository       = "https://helm.elastic.co"
   create_namespace = false
-  namespace        = local.elk_namespace
+  namespace        = local.efk_namespace
   chart            = "elasticsearch"
   version          = "8.5.1"
   values           = [file("./elasticsearch-values.yaml")]
-  depends_on = [ kubernetes_namespace.elk ]
+  depends_on = [ kubernetes_namespace.efk ]
 }
 
 resource "helm_release" "kibana" {
   name             = "kibana"
   repository       = "https://helm.elastic.co"
   create_namespace = false
-  namespace        = local.elk_namespace
+  namespace        = local.efk_namespace
   chart            = "kibana"
   version          = "8.5.1"
   values           = [file("./kibana-values.yaml")]
   depends_on = [ helm_release.elasticsearch ]
 }
 
-resource "helm_release" "logstash" {
-  name             = "logstash"
+resource "helm_release" "filebeat" {
+  name             = "filebeat"
   repository       = "https://helm.elastic.co"
   create_namespace = false
-  namespace        = local.elk_namespace
-  chart            = "logstash"
+  namespace        = local.efk_namespace
+  chart            = "filebeat"
   version          = "8.5.1"
-  values           = [file("./logstash-values.yaml")]
+  values           = [file("./filebeat-values.yaml")]
   depends_on = [ helm_release.elasticsearch ]
 }
